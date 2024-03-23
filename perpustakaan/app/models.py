cc75f678f82fdfa785f6fb0b3582e869398f8c99
mia29libra <mia29libra@gmail.com>
2024-03-23 12:38:41 +0700

    first commit

---
+++ perpustakaan/app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class webbuku (models.Model):
+    nama = models.CharField(max_length=50)
+    npm = models.CharField(max_length=50)
+    prodi = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}. {}'. format(self.id,self.nama)
