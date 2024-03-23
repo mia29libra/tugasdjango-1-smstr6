1229aa75099a80ef0df9559d312b54405375b238
mia29libra <mia29libra@gmail.com>
2024-03-23 19:40:10 +0700

    pembaruan form

--- perpustakaan/perpustakaan/urls.py
+++ perpustakaan/perpustakaan/urls.py
@@ -21,6 +21,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views. index),
+    path('', views.mia),
+    path('index/', views.index),
     path('api-auth/', include('rest_framework.urls'))
 ]
